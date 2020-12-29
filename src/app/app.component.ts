8afb8ec48f1cf88110b0de917168df2786849173
sinantayak <sinantayak@outlook.com>
2020-12-29 14:19:37 +0300

    Model ile ilgili örnek kodlar.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+/*tslint:disable*/
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  showSecret = false;
+  title = 'AngularTraining3';
+}
